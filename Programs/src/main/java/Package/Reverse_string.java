565451471592d3f258dfb6db7db2473bfe633bdd
Your Name <you@example.com>
2024-12-28 12:45:03 +0530

    v

---
+++ Programs/src/main/java/Package/Reverse_string.java
@@ -0,0 +1,26 @@
+package Package;
+
+public class Reverse_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ //String is immutable class, constant in nature we can't change the value directly
+		
+	//Using StringBuffer & StringBuilder class , directly we can use reverse() method.
+		StringBuilder strb=new StringBuilder("Swaroop Naralla");
+		System.out.println(strb);
+		
+		System.out.println(strb.reverse());
+		
+		
+		
+		//StringBuffer and StringBuilder both are mutable class
+		StringBuilder strb1=new StringBuilder("Swaroop Naralla");
+		System.out.println(strb1);
+		System.out.println(strb1.reverse());
+		
+		
+		
+	}
+
+}
