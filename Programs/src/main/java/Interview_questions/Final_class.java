33c778c26fe95660011364aa27618daa9d5d504d
Your Name <you@example.com>
2024-12-29 18:38:13 +0530

    use of final keyword with Class

---
+++ Programs/src/main/java/Interview_questions/Final_class.java
@@ -0,0 +1,13 @@
+package Interview_questions;
+
+final class Test{  //we can't extend this class
+	
+}
+public class Final_class  extends Test{
+  //extends Test problem with this
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
