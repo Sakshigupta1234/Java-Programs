26b0bb293b8d17c607acdda0ebff3b1d514f0ce8
Your Name <you@example.com>
2024-12-29 18:35:00 +0530

    Use of Final keyword with variable and method

---
+++ Programs/src/main/java/Interview_questions/FinaLkeyword.java
@@ -0,0 +1,25 @@
+package Interview_questions;
+
+
+ class Test{
+	final void show() {  // if i declare method as final 
+		//Use of final with method:- we can't override that method
+		
+	}
+}
+public class FinaLkeyword extends Test {
+//	void show() {
+//		// this block will throw error i.e. Cannot override the final method from Test
+//	}
+	public static void main(String[] args) {
+		
+//Final is a keyword which we can use with variable, method or Class.
+	
+		
+		 final int x=20; //Use of final keyword with variable :- value becomes constant/fix.
+		
+		
+		
+	}
+
+}
