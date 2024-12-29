ff9e70f3ee8909de453f6836cbe6eb429cf28d18
Your Name <you@example.com>
2024-12-29 11:33:18 +0530

    Local and Instance variable

---
+++ Programs/src/main/java/Day2/Variable_type.java
@@ -0,0 +1,33 @@
+package Day2;
+
+public class Variable_type {
+
+	
+	int x, y;  //instance variable
+	
+	// there are 2 types of variables in java
+	
+	// 1. Instance Variable - related to object memory (once object is created it will allocate some memory where these variables will get stored)
+			
+	// Local variables - related to methods
+	
+	
+	//Parameterized constructor
+	Variable_type(int a, int b) {
+		x=a;
+		y=b;
+		
+	}
+	void display() {
+		System.out.println(x);
+		System.out.println(y);
+	}
+			
+	public static void main(String[] args) {
+		Variable_type obj=new Variable_type(10,20);
+		obj.display();
+		
+		
+	}
+
+}
