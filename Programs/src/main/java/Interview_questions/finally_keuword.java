843e4f59144ef37d8916b05922824cfa54b3bdaa
Your Name <you@example.com>
2024-12-29 18:48:13 +0530

    Used of Finalize keyword

---
+++ Programs/src/main/java/Interview_questions/finally_keuword.java
@@ -0,0 +1,26 @@
+package Interview_questions;
+
+public class finally_keuword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//finally is a block which we can use either with try or try-catch block.
+		
+		try {
+			
+		}
+	
+	finally{
+//used to perform cleanup activity
+}
+		
+		// 2nd way of using with tr-catch
+		try {
+			
+		}catch(){
+			
+		}finally {
+			
+		}
+	
+}}
